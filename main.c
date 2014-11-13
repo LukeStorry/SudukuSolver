b5371658bb41334134d63a6de60c22bee64dfd60
Luke Storry <luke@lukestorry.co.uk>
2014-11-13 14:35:25 +0000

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+/* A suduko solver program in C
+ * input as much of a suduko as possible, and then the program will atempt to solve.
+ * a work in progress by Luke Storry
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main (void) {
+    return 0;
+}

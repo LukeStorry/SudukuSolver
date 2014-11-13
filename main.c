3ac42e9cd3022ab82ebd340c7b34cf9997eb04b0
Luke Storry <luke@lukestorry.co.uk>
2014-11-13 18:02:11 +0000

    filled in all main IO functions

--- main.c
+++ main.c
@@ -8,27 +8,76 @@
 #include <ctype.h>
 
 
-void inputSize(int *n, int *cols);
-int *inputSud(int *suduko, int n, int cols);
- 
+void inputSize(int *rows, int *cols);
+int *inputSuduko(int *suduko, int rows, int cols);
+void ouputLine(int cols);
+void inputRow(int cols);
 
 int main (void) {
-    int n, cols;
+    int rows, cols;
     int *suduko;
-    inputSize(&n,&cols);
+    inputSize(&rows,&cols);
     inputSud(suduko,n, cols);
  
     return 0;
 }
 
-void inputSize(int *n, int *cols) {
-    
-    
+void inputSize(int *rows, int *cols) {
+    unsigned short input;
+    printf("How many rows does the suduko have? ");
+    scanf(" %2u ",input);
+    *rows = input;
+    printf("How many columns does the suduko have? ");
+    scanf(" %2u ",input);
+    *cols = input;
 };
 
-int *inputSud(int *suduko, int n, int m) {
-    int ;
+int *inputSuduku(int *suduko, int rows, int cols) {
+    unsigned short i;
     suduko = malloc(n*m*sizeof(int));
-    printf("Please enter your suduko puzzle below, row by row");
-    for (i=0, i<)
-    intpoutrow(*)
+    printf("Please enter your suduko puzzle below, row by row:\n");
+    outputLine(cols);
+    for ( i=0 ; i<rows ; i++ ) {
+        suduko[i*cols] = inputRow(cols);
+        outputLine(cols)
+    };
+    Thanks!
+};
+
+void outputLine(int cols) { //perhaps add a second argument for double lines?
+    unsigned short i;
+    for ( i = 0 ; i<cols ; i++ ) {
+        printf("─");
+    }
+    printf("\n");
+};
+
+int *inputRow(int cols) {
+    unsigned short j, *row;
+    printf("|│")
+    for (j=0 ; j<cols ; j++) {
+        printf(" ")
+        scanf(" %2u ", row[j])
+        printf(" │")
+    };
+    printf("|")
+    return row;
+};
+
+void outputSuduku (int* suduko, int rows, int cols) {
+    unsigned short i;
+    outputLine(cols);
+    for ( i=0 ; i<rows ; i++ ) {
+        outputRow(suduku+i, cols);
+        outputLine(cols);
+    };
+};
+
+void outputRow(int* row, int cols) {
+    unsigned short j;
+    printf("||")
+    for (j=0 ; j<cols ; j++) {
+            printf(" %u |",row[j])
+        };
+    printf("|")
+};

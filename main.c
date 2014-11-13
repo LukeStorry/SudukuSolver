54849b297a2ae5de62d164501b2f026231fbabac
Luke Storry <luke@lukestorry.co.uk>
2014-11-13 18:18:26 +0000

    changed inconsistent "suduko" to "suduku"

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-/* A suduko solver program in C
- * input as much of a suduko as possible, and then the program will atempt to solve.
+/* A suduku solver program in C
+ * input as much of a suduku as possible, and then the program will atempt to solve.
  * a work in progress by Luke Storry
  */
  
@@ -8,16 +8,16 @@
 #include <ctype.h>
 
 void inputSize(int *rows, int *cols);
-void inputSuduku(int *suduko, int rows, int cols);
+void inputSuduku(int *suduku, int rows, int cols);
 void inputRow(int *row, int cols);
-void outputSuduku (int* suduko, int rows, int cols);
+void outputSuduku (int* suduku, int rows, int cols);
 void outputLine(int cols); 
 void outputRow(int* row, int cols);
 
 int main (void) {
-    int rows, cols, *suduko;
+    int rows, cols, *suduku;
     inputSize(&rows,&cols);
-    inputSuduku(suduko,rows, cols);
+    inputSuduku(suduku,rows, cols);
     printf("\n\n\n TestL: is this it? \n\n");
     outputSuduku(suduku,rows,cols);
     return 0;
@@ -25,21 +25,21 @@ int main (void) {
 
 void inputSize(int *rows, int *cols) {
     unsigned short input;
-    printf("How many rows does the suduko have? ");
+    printf("How many rows does the suduku have? ");
     scanf(" %2u ",input);
     *rows = input;
-    printf("How many columns does the suduko have? ");
+    printf("How many columns does the suduku have? ");
     scanf(" %2u ",input);
     *cols = input;
 };
 
-void inputSuduku(int *suduko, int rows, int cols) {
+void inputSuduku(int *suduku, int rows, int cols) {
     unsigned short i;
-    suduko = malloc(rows*cols*sizeof(int));
-    printf("Please enter your suduko puzzle below, row by row:\n");
+    suduku = malloc(rows*cols*sizeof(int));
+    printf("Please enter your suduku puzzle below, row by row:\n");
     outputLine(cols);
     for ( i=0 ; i<rows ; i++ ) {
-        inputRow(&(suduko[i*rows]),cols);
+        inputRow(&(suduku[i*rows]),cols);
         outputLine(cols);
     };
     printf("Thanks!");

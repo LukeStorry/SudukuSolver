e5c30a2b746d756ec57b830cc32d3646cc5c1936
LukeStorry <luke@lukestorry.co.uk>
2014-11-17 09:19:48 +0000

    removed last rows and cols, checked with grep

--- include/IO.h
+++ include/IO.h
@@ -1,11 +1,11 @@
 //header file for the functions that input and output sudukus from BASH* 
 
-void inputSize (US *size;
+void inputSize (US *size);
 void inputSuduku (US *suduku, US size);
 
-//static bool validateInput(US *ptr, US cols, US rows);
+//static bool validateInput(US *ptr, US max);
 
 void outputSuduku (US* suduku, US size);
-//static void outputLine(US cols); 
-//static void outputRow (US* row, US cols);
-//static void outputPartSuduku (US *suduku, US cols, US currentRow, US currentCol);
+//static void outputLine(US size); 
+//static void outputRow (US* row, US size);
+//static void outputPartSuduku (US *suduku, US size, US currentRow, US currentCol);

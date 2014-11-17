fbae09ec27fbb5687a52d45203c60eebb3c666b7
LukeStorry <luke@lukestorry.co.uk>
2014-11-17 09:18:16 +0000

    removed more rowcols and cleaned up counter variable names

--- include/IO.h
+++ include/IO.h
@@ -1,11 +1,11 @@
 //header file for the functions that input and output sudukus from BASH* 
 
-void inputSize (US *rows, US *cols);
-void inputSuduku (US *suduku, US rows, US cols);
+void inputSize (US *size;
+void inputSuduku (US *suduku, US size);
 
 //static bool validateInput(US *ptr, US cols, US rows);
 
-void outputSuduku (US* suduku, US rows, US cols);
+void outputSuduku (US* suduku, US size);
 //static void outputLine(US cols); 
 //static void outputRow (US* row, US cols);
 //static void outputPartSuduku (US *suduku, US cols, US currentRow, US currentCol);
--- include/solver.h
+++ include/solver.h
@@ -1,4 +1,4 @@
 /* This will store the declarations of the public solving functions.
  */
 
-int solveSuduku(US *suduku, US rows, US cols);
+int solveSuduku(US *suduku, US size);

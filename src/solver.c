dd10886beb8191363c4cc62c7f20ac4fb53269e7
LukeStorry <luke@lukestorry.co.uk>
2014-11-18 15:01:26 +0000

    solved some compile errors but bool not working

--- src/solver.c
+++ src/solver.c
@@ -3,11 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <bool.h>
+#include <stdbool.h>
 
 typedef unsigned short int US; // to save typing it out every time.
 
 void initialise (bool **possibles, US *input, US size);
+
 
 // This will be the main, and probably the only public, function.
 int solveSuduku(US *input, US size) {
@@ -24,19 +25,18 @@ int solveSuduku(US *input, US size) {
 
 
 
-void initialise(US **possibles, US *input, US size) {
+void initialise(bool **possibles, US *input, US size) {
     US i, j;
     for( i=0 ; i < size*size ; i++ ) {
 	possibles[i] = malloc(size*sizeof(bool)); //each cell pointer points to an array of bools
 	if (input[i] == 0) { //if input was blank
-	    for( j=0 ; j < size ; j++ )
+	    for( j=0 ; j < size ; j++ ) {
 		possibles[i][j] = true; //all numbers are possible
+	    };
 	} else {
-	    for( j=0 ; j < size ; j++ )
+	    for( j=0 ; j < size ; j++ ) {
 		possibles[i][j] = (input[i] = j+1) ? true : false; //only the input number is possible.
+	    };
 	};
     };
-}
-	    
-
 }

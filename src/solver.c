96985153c3c8220d0a54ec2b8deea174920beaff
LukeStorry <luke@lukestorry.co.uk>
2014-11-20 18:25:27 +0000

    added duplicate functions

--- src/solver.c
+++ src/solver.c
@@ -7,13 +7,17 @@
 
 typedef unsigned short int US; // to save typing it out every time.
 void initialise (bool **possibles, US *input, US size);
+void removeRowDuplicates(bool **possibles, US size);
+void removeColDuplicates(bool **possibles, US size);
+void removeBoxDuplicates(bool **possibles, US size);
 
 
 // This will be the main, and probably the only public, function.
 int solveSuduku(US *input, US size) {
     int errornum = 0;
-    bool **possibles = malloc(size*size*sizeof(bool)); //mallocs a pointer per cell.
+    bool **possibles = malloc(size*size*sizeof(bool*)); //mallocs a pointer per cell.
     initialise(possibles, input, size);
+void removeRowDuplicates(bool **possibles, US size);
 
     //call other solving functions.
 
@@ -23,7 +27,7 @@ int solveSuduku(US *input, US size) {
 }
 
 
-
+// This uses the input to initialise a 3d bool Array of all the possibilities
 void initialise(bool **possibles, US *input, US size) {
     US i, j;
     for( i=0 ; i < size*size ; i++ ) {
@@ -39,3 +43,17 @@ void initialise(bool **possibles, US *input, US size) {
 	};
     };
 }
+
+// This checks each row and changes the bool possibility tag 
+void removeRowDuplicates(bool **possibles, US size) {
+    int row;
+    for ( row = 0 ; row < size ; row++ ) {
+
+
+// This checks each column and changes the bool possibility tag 
+void removeColDuplicates(bool **possibles, US size);
+
+
+
+// This checks each box and changes the bool possibility tag 
+void removeBoxDuplicates(bool **possibles, US size);

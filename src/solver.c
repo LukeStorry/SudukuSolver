a9aeeb6b6ddbe6509a409af110ccc3ca50044b43
LukeStorry <luke@lukestorry.co.uk>
2014-11-20 18:47:22 +0000

    added Poss emun type

--- src/solver.c
+++ src/solver.c
@@ -3,21 +3,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <stdPoss.h>
 
 typedef unsigned short int US; // to save typing it out every time.
-void initialise (bool **possibles, US *input, US size);
-void removeRowDuplicates(bool **possibles, US size);
-void removeColDuplicates(bool **possibles, US size);
-void removeBoxDuplicates(bool **possibles, US size);
+typedef enum poss {notpossible, possible, definite} Poss;
+
+
+static void initialise (Poss **possibles, US *input, US size);
+static void checkRowDuplicates(Poss **possibles, US size);
+static void checkColDuplicates(Poss **possibles, US size);
+static void checkBoxDuplicates(Poss **possibles, US size);
 
 
 // This will be the main, and probably the only public, function.
 int solveSuduku(US *input, US size) {
     int errornum = 0;
-    bool **possibles = malloc(size*size*sizeof(bool*)); //mallocs a pointer per cell.
+    Poss **possibles = malloc(size*size*sizeof(Poss*)); //mallocs a pointer per cell.
     initialise(possibles, input, size);
-void removeRowDuplicates(bool **possibles, US size);
+static void checkRowDuplicates(Poss **possibles, US size);
 
     //call other solving functions.
 
@@ -27,33 +30,39 @@ void removeRowDuplicates(bool **possibles, US size);
 }
 
 
-// This uses the input to initialise a 3d bool Array of all the possibilities
-void initialise(bool **possibles, US *input, US size) {
+// This uses the input to initialise a 3d Poss Array of all the possibilities
+static void initialise(Poss **possibles, US *input, US size) {
     US i, j;
     for( i=0 ; i < size*size ; i++ ) {
-	possibles[i] = malloc(size*sizeof(bool)); //each cell pointer points to an array of bools
+	possibles[i] = malloc(size*sizeof(Poss)); //each cell pointer points to an array of Posss
 	if (input[i] == 0) { //if input was blank
 	    for( j=0 ; j < size ; j++ ) {
-		possibles[i][j] = true; //all numbers are possible
+		possibles[i][j] = possible; //all numbers are possible
 	    };
 	} else {
 	    for( j=0 ; j < size ; j++ ) {
-		possibles[i][j] = (input[i] = j+1) ? true : false; //only the input number is possible.
+		possibles[i][j] = (input[i] = j+1) ? definite : not possible; //the input number is definite, the rest aren't possible.
 	    };
 	};
     };
 }
 
-// This checks each row and changes the bool possibility tag 
-void removeRowDuplicates(bool **possibles, US size) {
-    int row;
-    for ( row = 0 ; row < size ; row++ ) {
+// This checks each row and changes the Poss possibility tag 
+static void checkRowDuplicates(Poss **possibles, US size) {
+    int i, row;
+    for ( row = 0 ; row < size ; row++ ) {//in each row,
+	for ( i = 0 ; i < size ; i++ ) { //for each number,
+
+	    
+
+// This checks each column and changes the Poss possibility tag 
+static void checkColDuplicates(Poss **possibles, US size) {
+    ;
+}
 
 
-// This checks each column and changes the bool possibility tag 
-void removeColDuplicates(bool **possibles, US size);
 
-
-
-// This checks each box and changes the bool possibility tag 
-void removeBoxDuplicates(bool **possibles, US size);
+// This checks each box and changes the Poss possibility tag 
+static void checkBoxDuplicates(Poss **possibles, US size) {
+    ;
+}

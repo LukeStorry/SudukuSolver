556bd44d25661f0aa9643985240fa89eeda3b3b2
LukeStorry <luke@lukestorry.co.uk>
2014-11-21 11:46:29 +0000

    capitalised enum and fixed some bugs

--- src/solver.c
+++ src/solver.c
@@ -3,10 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdPoss.h>
+#include <stdbool.h>
 
 typedef unsigned short int US; // to save typing it out every time.
-typedef enum poss {notpossible, possible, definite} Poss;
+typedef enum poss {NOTPOSSIBLE, POSSIBLE, DEFINITE} Poss;
 
 
 static void initialise (Poss **possibles, US *input, US size);
@@ -20,7 +20,7 @@ int solveSuduku(US *input, US size) {
     int errornum = 0;
     Poss **possibles = malloc(size*size*sizeof(Poss*)); //mallocs a pointer per cell.
     initialise(possibles, input, size);
-static void checkRowDuplicates(Poss **possibles, US size);
+    checkRowDuplicates(possibles, size);
 
     //call other solving functions.
 
@@ -37,11 +37,11 @@ static void initialise(Poss **possibles, US *input, US size) {
 	possibles[i] = malloc(size*sizeof(Poss)); //each cell pointer points to an array of Posss
 	if (input[i] == 0) { //if input was blank
 	    for( j=0 ; j < size ; j++ ) {
-		possibles[i][j] = possible; //all numbers are possible
+		possibles[i][j] = POSSIBLE; //all numbers are possible
 	    };
 	} else {
 	    for( j=0 ; j < size ; j++ ) {
-		possibles[i][j] = (input[i] = j+1) ? definite : not possible; //the input number is definite, the rest aren't possible.
+		possibles[i][j] = (input[i] = j+1) ? DEFINITE : NOTPOSSIBLE; //the input number is definite, the rest aren't possible.
 	    };
 	};
     };
@@ -65,7 +65,7 @@ static bool numIsInRow(US num, US row, US** suduku, US size) {
     US cell;
     bool taken = false;
     for ( cell = 0 ; cell < size ; cell++ ) { //for each cell in row,
-	if (possibilities[row*size+cell][num] = definite) { //if thenum is taken by that cell
+	if (possibilities[row*size+cell][num] == DEFINITE ) { //if thenum is taken by that cell
 	    taken = true;
 	};
     };
@@ -76,7 +76,7 @@ static void removeNumFromRow(US num, US row, US** suduku, US size) {
     US cell;
     bool taken = false;
     for ( cell = 0 ; cell < size ; cell++ ) { //for each cell in row,
-	possibilities[row*size+cell][num] = notpossible;
+	possibilities[row*size+cell][num] = NOTPOSSIBLE;
     };
 }
 
